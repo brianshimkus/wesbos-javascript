cd8f3c3901b335d43a894b4a83f32b98c97947da
Brian Shimkus <brian@Brians-MacBook-Pro.local>
2021-06-30 15:00:10 -0500

    Worked with null and undefined

---
+++ playground/types.js
@@ -0,0 +1,20 @@
+const person = {
+  first: 'brian',
+  last: 'shimkus',
+  age: 100
+}
+
+let dog;
+console.log(dog);
+dog = 'snickers';
+
+let undefined;
+const someNull = null;
+
+const teller = {
+  first: 'Raymond',
+  last: 'Teller'
+}
+
+teller.first = 'Teller';
+teller.last = null;
